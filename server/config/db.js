f4b4d3441aa82a159abec2beabf35fb567bd82ba
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2021-10-19 17:18:37 -0400

    mongo connection to mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://ikam:4Cl1JHjWIwfapAqq@mongodbserver.slo1c.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
